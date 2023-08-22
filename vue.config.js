13cb1034f71806c9bb220dd14da752d313330d0f
hanuuul <hanuuul.h@gmail.com>
2023-08-22 20:05:32 +0900

    frontend start

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //두단어 이상 조합하라고 하는 오류 막기
+  lintOnSave:false
 })
